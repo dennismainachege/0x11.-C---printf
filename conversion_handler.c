c40e868400a17ddc82f2c21c4ec655f803715117
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2023-11-13 12:32:32 +0300

    refactored code to include a conversion_handler

---
+++ conversion_handler.c
@@ -0,0 +1,48 @@
+#ifndef CONVERSION_HANDLER
+#define CONVERSION_HANDLER
+#include "main.h"
+
+void conversion_handler(const char *format, va_list extras)
+{
+    int i;
+    for (i = 0; format[i] != '\0'; i++)
+	{
+		switch (format[i])
+		{
+			case '%':
+				i++; /* Move to the next character after '%' */
+				switch (format[i])
+				{
+					case 'c':
+					{
+						int character = va_arg(extras, int);
+
+						printf("%c", character);
+						break;
+					}
+					case 's':
+					{
+						char *character_string = va_arg(extras, char *);
+
+						if (character_string != NULL)
+						{
+							printf("%s", character_string);
+						}
+						else
+						{
+							printf("(null)");
+						}
+						break;
+					}
+					default:
+						printf("%%");
+						break;
+				}
+				break;
+			default:
+				printf("%c", format[i]);
+				break;
+		}
+	}
+}
+#endif /* #ifndef CONVERSION_HANDLER*/
