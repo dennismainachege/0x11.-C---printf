f033b01c5df1db4479dc627d741f4604ef8ab836
dennismainachege <dennismainachege@gmail.com>
2023-11-13 20:40:53 -0800

    Updated version

--- _printf.c
+++ _printf.c
@@ -1,135 +1,66 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
-  * _printf - produces output according to a format
-  * @format: Character string
-  * @...: Extra arguments
-  * Return: The number of characters printed
-  */
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
-	int num_args;
-	int num_of_characters_printed;
-	va_list extras;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
-		return (0);
+		return (-1);
 
-	num_args = strlen(format);
-	if (num_args < 1)
-		return (0);
+	va_start(list, format);
 
-	va_start(extras, format);
-
-	num_of_characters_printed = conversion_handler(format, extras);
-	va_end(extras);
-
-	return (num_of_characters_printed);
-}
-
-int conversion_handler(const char *format, va_list extras)
-{
-	int i;
-
-	int count = 0;
-
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		switch (format[i])
+		if (format[i] != '%')
 		{
-			case '%':
-				i++; /* Move to the next character after '%' */
-				switch (format[i])
-				{
-					case 'c':
-					{
-						int character = va_arg(extras, int);
-
-						printf("%c", character);
-						break;
-					}
-					case 'i':
-					{
-						int num_i = va_arg(extras, int);
-
-						printf("%i", num_i);
-						break;
-					}
-					case 'd':
-					{
-						int num_d = va_arg(extras, int);
-
-						printf("%d", num_d);
-						break;
-					}
-					case 'u':
-					{
-						unsigned int num_u = va_arg(extras, int);
-
-						printf("%u", num_u);
-						break;
-					}
-					case 'o':
-					{
-						unsigned int num_o = va_arg(extras, int);
-
-						printf("%o", num_o);
-						break;
-					}
-					case 'x':
-					{
-						unsigned int num_x = va_arg(extras, int);
-
-						printf("%x", num_x);
-						break;
-					}
-					case 'X':
-					{
-						unsigned int num_X = va_arg(extras, int);
-
-						printf("%X", num_X);
-						break;
-					}
-					case 'p':
-					{
-						void *addr = va_arg(extras, void *);
-
-						printf("%p", addr);
-						break;
-					}
-					case '%':
-					{
-						count++;
-						putchar('%');
-						break;
-					}
-					case 's':
-					{
-						char *character_string = va_arg(extras, char *);
-
-						if (character_string != NULL)
-						{
-							printf("%s", character_string);
-						}
-						else
-						{
-							printf("(null)");
-						}
-						break;
-					}
-					default:
-					{
-						putchar('%');
-						printf("%c", format[i]);
-						break;
-					}
-				}
-				break;
-			default:
-				putchar(format[i]);
-				count++;
-				break;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
 	}
-	return (count);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
